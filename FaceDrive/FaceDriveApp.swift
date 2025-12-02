80f9f04296619fe83b9428c26c3eee83b4c0a6a8
CrowKing63 <choiuitaek@gmail.com>
2025-12-02 10:35:25 +0900

    Initial Commit

---
+++ FaceDrive/FaceDriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceDriveApp.swift
+//  FaceDrive
+//
+//  Created by 최의택 on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FaceDriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
